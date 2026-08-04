2a68cb0a69349bb906c6865c6fc467b88b98f8e1
YakiKawai <andrey.mel009@gmail.com>
2026-08-04 11:40:00 +0500

    chore(spy): убраны неиспользуемые импорты в SpyChatMerger

--- TMessagesProj/src/main/java/org/aurex/features/spy/SpyChatMerger.java
+++ TMessagesProj/src/main/java/org/aurex/features/spy/SpyChatMerger.java
@@ -7,11 +7,9 @@ import org.telegram.messenger.MessageObject;
 import org.telegram.messenger.UserConfig;
 import org.telegram.tgnet.TLRPC;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.WeakHashMap;
 
